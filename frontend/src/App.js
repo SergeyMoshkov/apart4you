8be43a418e3bac95417924f4e9a3061613eca41b
SergeyMoshkov <sergejmoshkov@gmail.com>
2022-07-23 17:04:21 +0300

    добавлен backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SecondComponents from './components/SecondComponets';
+
+
 
 function App() {
+  let apple = 1
+  function sumApple(n) {
+
+    if (n===1) {
+      return `У Сергея ${n} яблоко!`
+    }
+    else if (n < 0) {
+      return `Сергей отдал ${Math.abs(n)} яблок!`
+    }
+    else if (n===0 || n > 4) {
+      return `У Сергея ${n} яблок!`
+    }
+    else if (2 < n < 4) {
+      return `У Сергея ${n} яблока!`
+    }
+    
+    
+  }
+
+  function MyComponents() {
+    return (
+      <React.Fragment>
+        <h1 className='myClass'>{sumApple(apple)}</h1>
+        
+      </React.Fragment>
+      
+    )
+    
+  }
+  
   return (
     <div>
-      
+      <MyComponents />
+      <SecondComponents />
     </div>
   );
 }
